2845282975226931897551d18758fee314b72fbf
bhogesh4i9 <vurabhogesh@gmail.com>
2025-07-03 05:52:35 -0400

    render the functions of context

--- src/components/DeliveryAddressSection.jsx
+++ src/components/DeliveryAddressSection.jsx
@@ -1,17 +1,18 @@
 // src/components/checkout/DeliveryAddressSection.js
-import React from 'react';
+import React, { useState } from 'react';
 import { Accordion } from 'react-bootstrap';
 import AddressListAccordion from './AddressListAccordion';
 import AddressFormAccordion from './AddressFormAccordion';
 
-
 const DeliveryAddressSection = () => {
+  const [activeKey, setActiveKey] = useState('');
+
   return (
     <div className="container my-4">
       <h4 className="mb-4 fw-bold">1. Delivery Address</h4>
-      <Accordion alwaysOpen>
-        <AddressListAccordion />
-        <AddressFormAccordion />
+      <Accordion activeKey={activeKey} onSelect={(key) => setActiveKey(key)} alwaysOpen>
+        <AddressListAccordion activeKey={activeKey} setActiveKey={setActiveKey} />
+        <AddressFormAccordion activeKey={activeKey} setActiveKey={setActiveKey} />
       </Accordion>
     </div>
   );
