19433eb912d7a9023a28328fa64bcc0d38a41137
bhogesh4i9 <vurabhogesh@gmail.com>
2025-07-03 02:19:20 -0400

    fix: use dynamic basename for routing

--- src/main.jsx
+++ src/main.jsx
@@ -7,15 +7,12 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { BrowserRouter } from 'react-router-dom';
 import { ProductProvider } from './context/ProductContext.jsx';
 
-
 createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-       <BrowserRouter basename="/E-Commerce">
-       <ProductProvider>
-          <App />
-       </ProductProvider>
-      </BrowserRouter>
-    </React.StrictMode>
-   
-  
+  <React.StrictMode>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <ProductProvider>
+        <App />
+      </ProductProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 )
